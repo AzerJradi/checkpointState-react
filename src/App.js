6e0fe4c20aef9619e47c8b920102c41b71c90353
AzerJradi <azerjradi13@gmail.com>
2024-02-25 10:31:21 +0100

    add

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './App.css'; // Import the CSS file for styling
 class App extends Component {
   constructor(props) {
     super(props);
-
     // Initial state setup
     this.state = {
       person: {
@@ -19,7 +18,6 @@ class App extends Component {
       timeSinceMount: 0, // Time since the component was mounted
     };
   }
-
   componentDidMount() {
     // Set up the interval to update timeSinceMount every second
     const intervalId = setInterval(() => {
@@ -27,30 +25,24 @@ class App extends Component {
         timeSinceMount: prevState.timeSinceMount + 0.5, // Increment timeSinceMount every half-second
       }));
     }, 1000);
-
     this.setState({ intervalId });
   }
-
   componentWillUnmount() {
     // Clear the interval when the component is unmounted to avoid memory leaks
     clearInterval(this.state.intervalId);
   }
-
   // Toggle the show state when the button is clicked
   handleToggleShow = () => {
     this.setState((prevState) => ({
       show: !prevState.show,
     }));
   };
-
   render() {
     const { person, show, timeSinceMount } = this.state;
-
     return (
       <div>
         {/* Button to toggle the visibility of the person's profile */}
         <button onClick={this.handleToggleShow}>Toggle Show</button>
-
         {/* Render the person's profile if the show state is true */}
         {show && (
           <div>
@@ -60,7 +52,6 @@ class App extends Component {
             <p>Profession: {person.profession}</p>
           </div>
         )}
-
         {/* Display the time interval since the component was mounted */}
         <p>Messi's goals per second: {timeSinceMount} seconds</p>
       </div>
